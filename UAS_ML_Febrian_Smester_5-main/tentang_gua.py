91582c0eaba910ec95fb45116338f455a561080f
febrianhikmah <febriannurrohim12@gmail.com>
2026-05-18 00:13:43 +0700

    Delete UAS_ML_Febrian_Smester_5-main directory

--- UAS_ML_Febrian_Smester_5-main/tentang_gua.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-def contact_me():
-    st.write('**Contact Me**')
-    st.write('Jika Anda memiliki pertanyaan atau ingin berdiskusi lebih lanjut, silakan hubungi saya melalui email atau media sosial berikut:')
-    st.write('📧 Email : febriannurrohim12@gmail.com')
-    st.write('🔗 LinkedIn: (https://www.linkedin.com/in/febrian-nurrohim-0249602b5/)')
-    st.write('🐱 GitHub: (https://github.com/febrianhikmah)')
-    st.write('📷 Instagram: (https://www.instagram.com/rohimm.02/)')
-    st.write('Saya terbuka untuk kolaborasi dan diskusi seputar data science. Terima kasih telah mengunjungi aplikasi ini!')
